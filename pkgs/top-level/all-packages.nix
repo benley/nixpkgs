9f307a00451f47b64f5bfb847b1d17b7fdea84c5
Benjamin Staffin <ben@folsomlabs.com>
2015-02-19 16:38:40 -0800

    New package: prometheus-haproxy-exporter

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8274,6 +8274,8 @@ let
   prometheus = callPackage ../servers/monitoring/prometheus { };
   prometheus-alertmanager =
     callPackage ../servers/monitoring/prometheus/alertmanager { };
+  prometheus-haproxy-exporter =
+    callPackage ../servers/monitoring/prometheus/haproxy_exporter { };
 
   psqlodbc = callPackage ../servers/sql/postgresql/psqlodbc { };
 
