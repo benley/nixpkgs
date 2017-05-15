4ec6c498d369b1e7a14e39ee5e7654b9fc5d2263
Benjamin Staffin <benley@gmail.com>
2017-05-15 13:54:58 -0400

    kops: 1.4.0 -> 1.6.0-beta.1

--- pkgs/applications/networking/cluster/kops/default.nix
+++ pkgs/applications/networking/cluster/kops/default.nix
@@ -2,24 +2,30 @@
 
 buildGoPackage rec {
   name = "kops-${version}";
-  version = "1.4.0";
-  rev = "v${version}";
+  version = "1.6.0-beta.1";
 
   goPackagePath = "k8s.io/kops";
 
   src = fetchFromGitHub {
-    inherit rev;
+    rev = version;
     owner = "kubernetes";
     repo = "kops";
-    sha256 = "1jwgn7l8c639j5annwymqjdw5mcajwn58y21042jy5lhgdh8pdf5";
+    sha256 = "0fh8m6kxcahpgpphmrvbyvvxhv1fkq6lm8nj5vk754czr3hlggyn";
   };
 
   buildInputs = [go-bindata];
   subPackages = ["cmd/kops"];
 
+  buildFlagsArray = ''
+    -ldflags=
+        -X k8s.io/kops.Version=${version}
+        -X k8s.io/kops.GitVersion=${version}
+  '';
+
   preBuild = ''
     (cd go/src/k8s.io/kops
-     go-bindata -o upup/models/bindata.go -pkg models -prefix upup/models/ upup/models/...)
+     go-bindata -o upup/models/bindata.go -pkg models -prefix upup/models/ upup/models/...
+     go-bindata -o federation/model/bindata.go -pkg model -prefix federation/model federation/model/...)
   '';
 
   meta = with stdenv.lib; {
